7990c132462a36ec9939c1a406091325cc950a3d
dechant22413 <juliandechant05@gmail.com>
2026-05-03 22:52:37 +0200

    knife hitting logic
    wokring knife hitting skript

--- Assets/Scripts/Player/Weapons/PlayerKnife.cs
+++ Assets/Scripts/Player/Weapons/PlayerKnife.cs
@@ -2,6 +2,28 @@ using UnityEngine;
 
 public class PlayerKnife : MonoBehaviour
 {
+    [Header("References")]
+    public LayerMask attackLayer;
+    public Camera playerCam;
+
+    [Header("Knife Stats")]
+    public float attackDistance = 3f;
+    public float attackDamage;
+
+    public void Attack()
+    {
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out RaycastHit hit, attackDistance, attackLayer))
+        {
+            HitTarget(hit.point);
+        }
+        ;
+    }
+
+    private void HitTarget(Vector3 pos)
+    {
+        Debug.Log("Hit!");
+    }
+
     public void OnAnimationStart()
     {
         PlayerAnimations.Instance.IsLeftArmPlaying = true;
