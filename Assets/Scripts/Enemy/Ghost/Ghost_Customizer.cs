8a8ceadcbc34f3b9e714f43120626e7eb30ba302
dechant22413 <juliandechant05@gmail.com>
2026-05-22 12:10:59 +0200

    ghost customizer and colliders
    added ghost customizer skript for host mesh and added colliders to bones

--- Assets/Scripts/Enemy/Ghost/Ghost_Customizer.cs
+++ Assets/Scripts/Enemy/Ghost/Ghost_Customizer.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
 
-public class Ghost_Customizer : MonoBehaviour
+[ExecuteAlways]
+public class GhostCustomizer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum HostType { Host_001, Host_002, Host_003 }
+
+    [Header("Host Settings")]
+    public HostType hostType;
+
+    [Header("Host Meshes")]
+    public GameObject ghoulMesh_001;
+    public GameObject ghoulMesh_002;
+    public GameObject ghoulMesh_003;
+
+    private void OnValidate()
     {
-        
+        UnityEditor.EditorApplication.delayCall += ApplyCustomization;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ApplyCustomization()
     {
-        
+        if (this == null) return;
+
+        if (ghoulMesh_001 != null) ghoulMesh_001.SetActive(hostType == HostType.Host_001);
+        if (ghoulMesh_002 != null) ghoulMesh_002.SetActive(hostType == HostType.Host_002);
+        if (ghoulMesh_003 != null) ghoulMesh_003.SetActive(hostType == HostType.Host_003);
     }
 }
+
+
