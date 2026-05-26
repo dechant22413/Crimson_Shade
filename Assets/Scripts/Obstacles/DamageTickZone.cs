afc10d4298f6c03a38a049c939323d31559a5528
dechant22413 <juliandechant05@gmail.com>
2026-05-26 19:31:47 +0200

    added additional prefabs and interactables

---
+++ Assets/Scripts/Obstacles/DamageTickZone.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+public class DamageTickZone : MonoBehaviour
+{
+    #region Settings
+    [Header("References")]
+    public BoxCollider boxCollider;
+
+    [Header("Damage Tick Settings")]
+    [SerializeField] private int damage;
+    [SerializeField] private float tickRate;
+    #endregion
+
+    private Coroutine damageTickCoroutine;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if(damageTickCoroutine == null)
+            {
+                damageTickCoroutine = StartCoroutine(DamageTick());
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(damageTickCoroutine != null)
+            {
+                StopCoroutine(damageTickCoroutine);
+                damageTickCoroutine = null;
+            }
+        }
+    }
+
+    private IEnumerator DamageTick()
+    {
+        while(true)
+        {
+            PlayerStatsAndUIPanel.Instance.ChangeLifePoints(damage * -1);
+            yield return new WaitForSeconds(tickRate);
+        }
+    }
+}
